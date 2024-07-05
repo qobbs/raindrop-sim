387dd4f63bb0d4c3f2936bea6d25998d98d93f9d
Aaliyah Evans <amevans4@csuchico.edu>
2024-07-05 21:09:20 +0000

    sngp g gp gs h8[gr h]gw  g9wrt t79e

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
-$( document ).ready(function() {
-    $('#btn').click(function() {
-        window.alert("DID YOU JUST CLICK ME?!");
-    });
-});
+//my variables 
+    //generates random slots for the drops to be in
+let randX = Math.floor((Math.random() * 500) + 1);
+let z;
+    //creates the start and end of the canvas
+let start = 1;
+let end = 1;
+
+//this creates my canvas
+function setup() {
+    createCanvas(500, 500);
+    frameRate(50);
+}//end of setup func
+
+//this generates raindrops
+function draw(){
+    //the sky (background)
+    background(8, 23, 155);
+    Text("💧", randX, 0);
+    //this will place random drops on the canvas
+        
+    }//end of if
+}//end of draw func
